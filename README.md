f80a31bca70a191f3ed35d2d5de14a3830cb6da9
Kanwar Ujjaval Singh <ujjavalkanwar@gmail.com>
2014-07-12 00:22:26 +0530

    first version

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ TwitterMiner
 ============
 
 Twitter Live Stream Tweets Mining Script in Node.js
+---------------------------------------------------
+
+Simple script to mine the live Twitter Stream API V1.1
+
+You can apply geographic, track and other filters.
+
+Takes arguments filenam
+
+`node index.js output`
+
+Will give output in output.json format
---
+++ index.js
@@ -0,0 +1,43 @@
+var twitter = require('ntwitter');
+var fs = require('fs');
+
+var outputFilename = process.argv[2]+'.json' || "data.json";
+var tweets=0;
+
+process.on('SIGINT', function() {
+    console.log("\nBye Bye");
+    process.exit();
+});
+
+process.on('uncaughtException', function(err) {
+    console.log('\nCaught exception: ' + err);
+    process.exit();
+});
+
+var twit = new twitter({
+  consumer_key: '6a0SyEIYoIShBqKE7IwUrd8JN',
+  consumer_secret: 'R30kr6kXUw7pBCWJChiIXFtTqD15BYeYdlMgiRPiHKgbZcXvZO',
+  access_token_key: '980420581-sQcLtAXR1ZaNkbrazvahetaIAD24PPLEt6mnBgkp',
+  access_token_secret: '8jp5bpeDCXlk506wHxoQMIx0TvJ2EzNowu42CaIWdilmb'
+});
+
+twit
+  .verifyCredentials(function (err, data){
+  	console.log("Verified and Connected to Twitter, now starting the writes to file " + "\"" +outputFilename + "\"");
+  })
+  .stream('statuses/filter', {'track':'GERvsARG, GermanyvsArgentina,Germany,Argentina,Romero,Zabaleta,Demichelis,Gago,di María,Mascherano,lvarez,Higuaín,Lionel,Messi,Agüero,Lavezzi,Neuer,Hummels,Lahm,Mertesacker,Boateng,Mustafi,Khedira,chweinsteiger,Özil,Schürrle,Podolski,Müller,Draxler,Kroos,Götze,Klose'}, function(stream) {
+  stream.on('data', function (data) {
+  	if(data.id && data.text){
+  	fs.appendFile(outputFilename, JSON.stringify(data)+'\n' , function(err) 
+  		{
+  			if(err){
+  				console.log("error encountered" + err);
+  			}
+  			else{
+  				tweets=tweets + 1;
+  				process.stdout.write("Written " + tweets + " tweets to " + outputFilename +" file\033[0G");
+  			}
+  		});
+  }
+  	});
+});
